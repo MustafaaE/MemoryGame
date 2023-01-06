e79b7d6925bbf2463a8c3889382d8ee77990c7da
Mustafa Elomeiri <Mustafaelomeiri@hotmail.se>
2023-01-06 14:34:25 +0100

    placeholder settings in place

--- src/Constants.js
+++ src/Constants.js
@@ -1,2 +1,10 @@
 export const DIFFICULTY = ["easy", "medium", "hard"];
 export const CARDS_COUNT = 8;
+export const DEFAULT_SEARCH = [
+  "cars",
+  "books",
+  "train",
+  "people",
+  "animals",
+  "nature",
+];
--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,18 +1,23 @@
 import { useState } from "react";
-import { DIFFICULTY, CARDS_COUNT } from "../../Constants";
+import { DIFFICULTY, CARDS_COUNT, DEFAULT_SEARCH } from "../../Constants";
 
 import Radiobox from "../RadioBox/Radiobox";
+
+const getRandomSearch = () => Math.round(Math.random() * (5 - 1) + 1);
 
 const Settings = () => {
   const [difficulty, setDifficulty] = useState(DIFFICULTY[1]);
   const [cardsCount, setCardsCount] = useState(CARDS_COUNT);
+  const [searchTerm, setSearchTerm] = useState(
+    DEFAULT_SEARCH[getRandomSearch()]
+  );
 
   return (
     <div className="settings">
       <h2>settings</h2>
 
       <h4>difficulty:</h4>
-      <div>
+      <div className="radiobox">
         {DIFFICULTY.map((item) => (
           <Radiobox
             key={item}
@@ -22,6 +27,28 @@ const Settings = () => {
           />
         ))}
       </div>
+
+      <div className="search-field">
+        <label htmlFor="search">Type theme </label>
+        <input
+          type="search"
+          id="search"
+          name="search"
+          defaultValue={searchTerm}
+        />
+      </div>
+
+      <div className="username-field">
+        <label htmlFor="username">Username </label>
+        <input
+          type="username"
+          id="username"
+          name="username"
+          placeholder="username"
+        />
+      </div>
+
+      <button className="start-button"> Start</button>
     </div>
   );
 };
